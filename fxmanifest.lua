d09004c3d7b265a4934d4690197ff4e922aa5c8d
TheFlyBandit <102429379+TheFlyBandit@users.noreply.github.com>
2024-05-29 22:38:58 -0400

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'TheFlyBandit'
+description 'Custom Black Postal Map'
+discord 'https://discord.gg/theflybanditstudio'
+
+this_is_a_map 'yes'
+
+client_script "client.lua"
